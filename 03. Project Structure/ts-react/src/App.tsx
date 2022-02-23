d85503e734094824f71778a418939a1494045d83
MuktadirHassan <tamimmuktadir@gmail.com>
2022-02-23 22:14:20 +0600

    nosto kore dilam

--- 03. Project Structure/ts-react/src/App.tsx
+++ 03. Project Structure/ts-react/src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import { useState } from "react";
 import TheLayout from "./views/pages/TheLayout";
-
 export const App = () => {
+       const [state, setState] = useState();
   return (
     <div>
       <TheLayout />
